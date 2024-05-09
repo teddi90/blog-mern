1751a82a7e4e75ba1031f57ce952ce57ca2aa8e0
teddi90 <slobodyanoleg5@gmail.com>
2024-05-09 12:53:05 +0300

    add cors allow

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import multer from "multer";
+import cors from "cors";
 import 'dotenv/config';
 
 import { registerValidation, loginValidation, postCreateValidation } from "./validation.js";
@@ -30,6 +31,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 // User
